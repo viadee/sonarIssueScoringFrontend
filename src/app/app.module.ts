e8028e5415264493f27859d500d7cb04a670293e
Yggdrahl <denniskra159@web.de>
2019-11-04 10:52:21 +0100

    First implementation of the navigation/routing-system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+//import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //DashboardComponent,
+    routingComponents,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
