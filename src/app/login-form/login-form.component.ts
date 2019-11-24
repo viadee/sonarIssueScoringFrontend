6f47cb722894f216cabd39a6a7b4069192505cf7
Yggdrahl <denniskra159@web.de>
2019-11-24 01:48:48 +0100

    Created "Branch"-class (Object) and added small test-function in the login-component

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -13,26 +13,71 @@ export class LoginFormComponent implements OnInit {
   repositories: Observable<any>;
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  repos;
+
+
+  //Test-------------------
+  branch1:Branch = new Branch("master");
+  branch2:Branch = new Branch("SNAPSHOT");
+  branchArray = [this.branch1, this.branch2];
+  testVar:Repository = new Repository(1, "viadee", this.branchArray);
+
+  ngOnInit() {
+    /*
+    var ausgabe:Branch[];
+    console.log(this.testVar.getBranches()[0].getName());
+    */
+  }
+
+  showRepo() {
+    console.log(this.repos[0].getBranches()[0].getName());
+  }
+  
+  
+  
+  //---------------------------
 
   onConnectToGithub() {
+    
     this.http
-      .get("/api/server/git-repo?username=trnhan251")
-      .subscribe(repos => console.log(repos));
-  }
+      .get<Repository[]>("/api/server/git-repo?username=" + this.authUsername)
+      .subscribe(repos => {
+        //console.log(repos)
+        this.repos = repos;
+      });
+  } 
 }
 
 class Repository {
   private id: number;
   private name: string;
+  private branches: Branch[];
+  
 
-  constructor(id: number, name: string) {
+  constructor(id: number, name: string, branches:Branch[]) {
     this.id = id;
     this.name = name;
+    this.branches = branches;
   }
 
   getId() {
     return this.id;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getBranches() {
+    return this.branches;
+  }
+}
+
+class Branch {
+  private name: string;
+
+  constructor(name : string) {
+    this.name = name;
   }
 
   getName() {
